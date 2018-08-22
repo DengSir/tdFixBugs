4a19064774d5c8ee5e743deb09e8b83000caa6a1
DengSir <tdaddon@163.com>
2018-08-23 01:35:06 +0800

    update

---
+++ Guild.lua
@@ -0,0 +1,7 @@
+-- Guild.lua
+-- @Author : DengSir (tdaddon@163.com)
+-- @Link   : https://dengsir.github.io
+-- @Date   : 8/23/2018, 1:34:30 AM
+
+GuildControlUIRankSettingsFrameRosterLabel = CreateFrame('Frame', nil, UIParent)
+GuildControlUIRankSettingsFrameRosterLabel:Hide()
