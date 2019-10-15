6bdca538be1c016caa003788dd585419552089be
Dencer <tdaddon@163.com>
2019-10-16 00:02:37 +0800

    update

--- Link.bat
+++
@@ -1,7 +0,0 @@
-%~d0
-cd %~dp0AddOns
-
-for /d %%i in (*) do (
-    rd "..\..\%%i"
-    mklink /d "..\..\%%i" "%cd%\%%i"
-)
--- WorldMap.lua
+++
@@ -1,7 +0,0 @@
---[[
-Scrap.lua
-@Author  : DengSir (tdaddon@163.com)
-@Link    : https://dengsir.github.io
-]]
-
-WorldMapTooltip.BackdropFrame = WorldMapTooltip
